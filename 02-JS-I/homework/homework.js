f30e5e425fabdc10538406d0492e06724a71fa4a
alexisalejio1 <alexisalejio1@gmail.com>
2022-04-14 03:21:07 -0500

    mod2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "holaxd";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 19;
+const nuevoNum = 25;
 
 // Crea una variable booleana:
 const nuevoBool = true;
